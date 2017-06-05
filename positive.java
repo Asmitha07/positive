d8e11b788e4d0ed9263a96c03339b719f524e9a6
Asmitha07 <asmitharenu@gmail.com>
2017-06-05 17:59:36 +0530

    Update positive.java

--- positive.java
+++ positive.java
@@ -1,40 +1,24 @@
-import java.io.*;
-
+ import java.io.*;
 import java.util.*;
-
 class positive
 {
-
 public static void main(String args[])
 {
-
-
 Scanner sc=new Scanner(System.in);
-
-int a=sc.nextInt;
-
-if(a%2!==0)
-
+int a=sc.nextInt();
+if(a%2!=0)
 {
-
 System.out.println("odd");
-
 }
-
 else if(a==0)
-
 {
-
 System.out.println("zero");
-
 }
-
 else
-
 {
+    System.out.println("even");
+}
+}
+}
 
-System.out.println("even");
 
-}
-}
-}
