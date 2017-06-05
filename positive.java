c98fdf2db1aa13e82c201dd56864c46c329d9754
Asmitha07 <asmitharenu@gmail.com>
2017-06-05 18:02:22 +0530

    Update positive.java

--- positive.java
+++ positive.java
@@ -6,9 +6,9 @@ public static void main(String args[])
 {
 Scanner sc=new Scanner(System.in);
 int a=sc.nextInt();
-if(a%2!=0)
+if(a>0)
 {
-System.out.println("odd");
+System.out.println("positive");
 }
 else if(a==0)
 {
@@ -16,7 +16,7 @@ System.out.println("zero");
 }
 else
 {
-    System.out.println("even");
+    System.out.println("negative");
 }
 }
 }
