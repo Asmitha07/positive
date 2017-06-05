356cfcf610017b953a2d48f97f4828ec0e7ae77a
Asmitha07 <asmitharenu@gmail.com>
2017-06-05 15:21:19 +0530

    Create positive.java

---
+++ positive.java
@@ -0,0 +1,40 @@
+import java.io.*;
+
+import java.util.*;
+
+class positive
+{
+
+public static void main(String args[])
+{
+
+
+Scanner sc=new Scanner(System.in);
+
+int a=sc.nextInt;
+
+if(a!==0)
+
+{
+
+System.out.println("odd");
+
+}
+
+else if(a==0)
+
+{
+
+System.out.println("zero");
+
+}
+
+else
+
+{
+
+System.out.println("even");
+
+}
+}
+}
