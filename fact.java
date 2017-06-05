98897409f347a4ccf6c65c63c7ac9eca1686c4cc
Asmitha07 <asmitharenu@gmail.com>
2017-06-05 17:46:58 +0530

    Create fact.java

---
+++ fact.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+class Fac
+{
+    public static void main(String args[])
+    {
+     Scanner sc=new Scanner(System.in);
+     int a=sc.nextInt();
+     int fact=1;
+     int i;
+     if(a!=0)
+     {
+         for(i=1;i<=a;i++)
+         fact=fact*i;
+     }
+     System.out.println(fact);
+    }
+}
