aff67455d445c20b85cc2b5d089cfa67ada0036b
Asmitha07 <asmitharenu@gmail.com>
2017-06-06 14:20:15 +0530

    Create strint.java

---
+++ strint.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+class strint
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        String a=sc.next();
+        int b=Integer.parseInt(a);
+        System.out.println(b);
+            
+    }
+}
+
